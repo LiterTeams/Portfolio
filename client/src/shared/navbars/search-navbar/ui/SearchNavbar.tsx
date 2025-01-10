9b6c48f967fc06c4f9bf40fbfc5a72e05ca4a949
LiterTeams <LiterTeams@gmail.com>
2025-01-10 16:19:00 +0300

    Update SearchNavbar.tsx

--- client/src/shared/navbars/search-navbar/ui/SearchNavbar.tsx
+++ client/src/shared/navbars/search-navbar/ui/SearchNavbar.tsx
@@ -5,6 +5,8 @@ export default function SearchNavbar({className}:{className?: string;}){
 
     const { searchParams, pathname, handleSearch } = useSearch();
 
+    console.log(pathname);
+
     return(
         <div className={`flex items-center gap-4 ${className}`}>
             <input className="flex-grow text-sm px-3 py-2 rounded-xl bg-dark-secondaty outline-none" placeholder="Search..." onChange={(e) => handleSearch(e.target.value)} defaultValue={searchParams.get("query")?.toString()} />
