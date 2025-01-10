3c80d66ade92296db74c809bc59c0ef67190c8ea
LiterTeams <LiterTeams@gmail.com>
2025-01-11 02:03:26 +0300

    Update AboutPageView.tsx

--- client/src/views/AboutPageView.tsx
+++ client/src/views/AboutPageView.tsx
@@ -1,8 +1,20 @@
 "use client";
+import Developer from "@widgets/personal-info/ui/Developer";
+import TechnologyStack from "@widgets/personal-info/ui/TechnologyStack";
+import Socials from "@widgets/personal-info/ui/Socials";
+import Repository from "@widgets/personal-info/ui/Repository";
 
 export default function AboutPageView(){
     return(
         <>
+            <div className="flex gap-6">
+                <Developer />
+                <div className="flex flex-col gap-6 flex-grow">
+                    <TechnologyStack />
+                    <Socials className="h-full" />
+                </div>
+            </div>
+            <Repository className="mt-6" />
         </>
     )
 }
